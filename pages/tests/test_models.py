600c0cd019051b0a44e707a2e09ee63f090faa48
Reza shakeri <rzashakeri@gmail.com>
2023-07-17 05:18:38 +0330

    ✅ test: added @pytest.mark.django_db to test_str_method

--- pages/tests/test_models.py
+++ pages/tests/test_models.py
@@ -1,4 +1,6 @@
 """tests for page app"""
+import pytest
+
 from .fixtures import page_name, page_slug
 from ..models import PageModel
 
@@ -6,6 +8,7 @@ from ..models import PageModel
 class TestPageModel:
     """test for the page model"""
 
+    @pytest.mark.django_db
     def test_str_method(self):
         """test for str method"""
 
