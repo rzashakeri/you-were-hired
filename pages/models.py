a53836340111e1a1cca2380d6cdc3eea18fa6729
Reza shakeri <rzashakeri@gmail.com>
2023-07-17 04:33:35 +0330

    ✨ feat: added PageModel

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
+"""
+pages app model
+"""
 from django.db import models
 
-# Create your models here.
+
+class PageModel(models.Model):
+    """Page Model"""
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
