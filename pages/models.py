facaed7d8875a519e739881e57d535b067b8cee5
Reza shakeri <rzashakeri@gmail.com>
2023-07-17 05:18:01 +0330

    ♻️ refactor: added Meta class for PageModel

--- pages/models.py
+++ pages/models.py
@@ -10,5 +10,9 @@ class PageModel(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
 
+    class Meta:
+        verbose_name = "page"
+        verbose_name_plural = "pages"
+
     def __str__(self):
         return str(self.name)
