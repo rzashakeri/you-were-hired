1daddcdbc584aa70307fdea77360bfffc25ee594
Reza shakeri <rzashakeri@gmail.com>
2023-07-19 23:58:25 +0330

    ✨ feat: added jobs app

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,6 @@
+"""pages url"""
 from django.urls import path
 
 from pages.views import HomeView
 
-urlpatterns = [
-	path("", HomeView.as_view(), name="home")
-]
+urlpatterns = [path("", HomeView.as_view(), name="home")]
