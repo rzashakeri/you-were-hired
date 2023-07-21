3f7266cb3fc3ca4896e26b3a88cf50f08074ae14
Reza shakeri <rzashakeri@gmail.com>
2023-07-21 16:56:04 +0330

    ✨ feat: added JobsView

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class JobsView(View):
+	def get(self, request):
+		return render(request, "jobs/jobs.html")
