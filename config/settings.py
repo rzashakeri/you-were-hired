37e0db381b64574694d6e8c574bbe71702fc7106
Reza shakeri <rzashakeri@gmail.com>
2023-07-24 23:42:37 +0330

    ♻️ refactor: changed TEMPLATES DIRS

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
 	{
 		"BACKEND": "django.template.backends.django.DjangoTemplates",
-		"DIRS": [BASE_DIR / 'templates']
+		"DIRS": [BASE_DIR / 'theme' / 'templates']
 		,
 		"APP_DIRS": True,
 		"OPTIONS": {
