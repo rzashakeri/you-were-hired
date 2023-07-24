091ad963675aff353546e71700e97bcdc0314840
Reza shakeri <rzashakeri@gmail.com>
2023-07-24 23:41:40 +0330

    ✨ feat: added addClassOnScroll to shared.js

--- theme/static/js/shared.js
+++ theme/static/js/shared.js
@@ -6,3 +6,20 @@ function collapse(obj) {
         element.classList.add("collapse-open");
     }
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const targetElement = document.querySelector('#header');
+
+    // Function to add the class when scrolling starts
+    function addClassOnScroll() {
+        if (window.scrollY > 0) {
+            targetElement.classList.add('shadow-sm');
+        } else {
+            targetElement.classList.remove('shadow-sm');
+        }
+    }
+
+    // Event listener for scroll event
+    window.addEventListener('scroll', addClassOnScroll);
+});
