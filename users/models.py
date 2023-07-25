636a12c3f877980f9cce2263bf4bb27f1c39bb72
Reza shakeri <rzashakeri@gmail.com>
2023-07-26 00:05:48 +0330

    ✨ feat: added UsertypeModel

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UsertypeModel(models.Model):
+	"""This Model To determine the user is a job seeker or a recruiter"""
+	
+	user_type_name = models.CharField(max_length=100)
