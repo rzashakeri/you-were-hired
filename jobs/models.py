ebf1a2a911735aa186cbc023a5f9691d2827fd89
Reza shakeri <rzashakeri@gmail.com>
2023-07-26 19:31:29 +0330

    ✨ feat: added JobType model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class JobType(models.Model):
+    """Job Type Model"""
+    name = models.CharField(max_length=255)
+    
+    class Meta:
+        # pylint: disable=too-few-public-methods
+        # pylint: disable=missing-class-docstring
+        
+        db_table = "job_type"
+        verbose_name = "job type"
+        verbose_name_plural = "job types"
