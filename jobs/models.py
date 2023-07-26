afd3386cde0fbfbd1aafd1cd93a74b30f9a9b096
Reza shakeri <rzashakeri@gmail.com>
2023-07-26 23:57:31 +0330

    ✨ feat: added JobPostSkill model

--- jobs/models.py
+++ jobs/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from cities_light.models import City
 from cities_light.models import Region
 from cities_light.models import Country
-from users.models import User, Company
+from users.models import User, Company, Skill
 from smart_selects.db_fields import ChainedForeignKey
 
 
 class JobType(models.Model):
     """Job Type Model"""
-    
+
     name = models.CharField(max_length=255)
-    
+
     class Meta:
         # pylint: disable=too-few-public-methods
         # pylint: disable=missing-class-docstring
-        
+
         db_table = "job_type"
         verbose_name = "job type"
         verbose_name_plural = "job types"
@@ -22,7 +22,7 @@ class JobType(models.Model):
 
 class JobPost(models.Model):
     """Job Post-Model"""
-    
+
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
     job_type = models.ForeignKey(JobType, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
@@ -30,11 +30,11 @@ class JobPost(models.Model):
     job_description = models.TextField()
     job_location = models.ForeignKey("JobLocation", on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
-    
+
     class Meta:
         # pylint: disable=too-few-public-methods
         # pylint: disable=missing-class-docstring
-        
+
         db_table = "job_post"
         verbose_name = "job post"
         verbose_name_plural = "job posts"
@@ -42,7 +42,7 @@ class JobPost(models.Model):
 
 class JobLocation(models.Model):
     """Job Location Model"""
-    
+
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     region = ChainedForeignKey(
         Region, chained_field="country", chained_model_field="country"
@@ -51,11 +51,11 @@ class JobLocation(models.Model):
         City, chained_field="country", chained_model_field="country"
     )
     zip = models.CharField(max_length=100, null=True, blank=True)
-    
+
     class Meta:
         # pylint: disable=too-few-public-methods
         # pylint: disable=missing-class-docstring
-        
+
         db_table = "job_location"
         verbose_name = "job location"
         verbose_name_plural = "job locations"
@@ -63,7 +63,7 @@ class JobLocation(models.Model):
 
 class JobPostActivity(models.Model):
     """Job Post-Activity Model"""
-    
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     job = models.ForeignKey(JobPost, on_delete=models.CASCADE)
     apply_date = models.DateTimeField()
@@ -71,7 +71,23 @@ class JobPostActivity(models.Model):
     class Meta:
         # pylint: disable=too-few-public-methods
         # pylint: disable=missing-class-docstring
-    
+
         db_table = "job_post_activity"
         verbose_name = "job activity"
         verbose_name_plural = "job activities"
+
+
+class JobPostSkill(models.Model):
+    """Job Post-Skill Model"""
+
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    job_post = models.ForeignKey(JobPost, on_delete=models.CASCADE)
+    skill_level = models.IntegerField()
+
+    class Meta:
+        # pylint: disable=too-few-public-methods
+        # pylint: disable=missing-class-docstring
+
+        db_table = "job_post_skill"
+        verbose_name = "job skill"
+        verbose_name_plural = "job skills"
