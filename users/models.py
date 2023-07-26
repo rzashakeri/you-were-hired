5c33bd1d4e73e9f8d61210243d1d90cbe8e52a47
Reza shakeri <rzashakeri@gmail.com>
2023-07-26 15:12:35 +0330

    ✨ feat: added JobSeekerProfile model

--- users/models.py
+++ users/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.conf import settings
 from birthday import BirthdayField, BirthdayManager
 from phone_field import PhoneField
+from djmoney.models.fields import MoneyField
 
 
 class UserType(models.Model):
     """This Model To determine the user is a job-seeker or a recruiter"""
 
+    # pylint: disable=too-few-public-methods
+
     user_type_name = models.CharField(max_length=100)
 
     class Meta:
+        # pylint: disable=too-few-public-methods
+        # pylint: disable=missing-class-docstring
         db_table = "user_type"
         verbose_name = "user type"
         verbose_name_plural = "user types"
@@ -18,6 +22,8 @@ class UserType(models.Model):
 
 class User(AbstractUser):
     """User Model"""
+
+    # pylint: disable=too-few-public-methods
 
     user_type = models.ForeignKey(UserType, on_delete=models.CASCADE)
     user_image = models.ImageField(upload_to="user/profile/", null=True, blank=True)
@@ -31,6 +37,20 @@ class User(AbstractUser):
     last_apply_job_date = models.DateTimeField(null=True, blank=True)
 
     class Meta:
+        # pylint: disable=too-few-public-methods
+        # pylint: disable=missing-class-docstring
+
         db_table = "user"
         verbose_name = "user"
         verbose_name_plural = "users"
+
+
+class JobSeekerProfile(models.Model):
+    """Job Seeker Profile Model"""
+
+    # pylint: disable=too-few-public-methods
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    current_salary = models.IntegerField()
+    is_annually_monthly = models.BooleanField(default=False)
+    currency = MoneyField(max_digits=14, decimal_places=2, default_currency="USD")
+
