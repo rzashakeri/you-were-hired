8cce4a0769627213d22d15c44a621665a87fa016
Reza shakeri <rzashakeri@gmail.com>
2023-07-26 17:57:38 +0330

    ✨ feat: added djmoney to INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 	'allauth.socialaccount',
 	'allauth.socialaccount.providers.google',
 	'allauth.socialaccount.providers.github',
+	'djmoney',
 	# internal apps
 	"pages",
 	"users"
