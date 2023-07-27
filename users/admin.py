f510615b9debe868e7b77acae4cf47eecffd3e55
Reza shakeri <rzashakeri@gmail.com>
2023-07-27 15:56:15 +0330

    ✨ feat: added SeekerProfileAdmin to admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
+from users.models import UserType, User, SeekerProfile
 
-from users.models import UserType, User
 
-
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    """User Model Admin"""
+    """User Admin"""
     list_display = ["username", "first_name", "last_name", "user_type", "is_staff", "is_superuser"]
     list_filter = ["is_staff", "is_superuser", "user_type"]
+    search_fields = ["first_name", "last_name", "username"]
 
 
-admin.site.register(User, UserAdmin)
+@admin.register(UserType)
+class UserTypeAdmin(admin.ModelAdmin):
+    """User Type Admin"""
+    pass
 
-admin.site.register(UserType)
 
+@admin.register(SeekerProfile)
+class SeekerProfileAdmin(admin.ModelAdmin):
+    """Seeker Profile Admin"""
+    list_display = ["__str__", "birthday", "current_salary"]
