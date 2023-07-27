1aab4a34e1b1f9ed12448f5457bb239b381ee225
Reza shakeri <rzashakeri@gmail.com>
2023-07-27 15:16:01 +0330

    ✨ feat: added UserAdmin and User to admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import UserType, User
+
+
+class UserAdmin(admin.ModelAdmin):
+    """User Model Admin"""
+    list_display = ["username", "first_name", "last_name", "user_type", "is_staff", "is_superuser"]
+    list_filter = ["is_staff", "is_superuser", "user_type"]
+
+
+admin.site.register(User, UserAdmin)
