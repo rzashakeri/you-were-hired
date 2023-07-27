5d3eab89968584ce3636ebc226fedf54ccb5cb38
Reza shakeri <rzashakeri@gmail.com>
2023-07-27 16:53:16 +0200

    ✨ feat: added EducationDetailAdmin to admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from users.models import UserType, User, SeekerProfile, SeekerSkill, Skill
+from users.models import UserType, User, SeekerProfile, SeekerSkill, Skill, EducationDetail
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """User Admin"""
     list_display = ["username", "first_name", "last_name", "gender", "user_type", "is_staff", "is_superuser"]
-    list_filter = ["is_staff", "is_superuser", "user_type"]
+    list_filter = ["is_staff", "is_superuser", "user_type", "gender", "date_joined"]
     search_fields = ["first_name", "last_name", "username"]
 
 
@@ -19,7 +19,8 @@ class UserTypeAdmin(admin.ModelAdmin):
 @admin.register(SeekerProfile)
 class SeekerProfileAdmin(admin.ModelAdmin):
     """Seeker Profile Admin"""
-    list_display = ["__str__", "birthday", "current_salary"]
+    list_display = ["__str__", "level", "birthday", "current_salary"]
+    list_filter = ["level", "birthday", "current_salary"]
 
 
 @admin.register(SeekerSkill)
@@ -30,4 +31,11 @@ class SeekerSkillAdmin(admin.ModelAdmin):
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
+    """Skill Admin"""
     pass
+
+
+@admin.register(EducationDetail)
+class EducationDetailAdmin(admin.ModelAdmin):
+    """Education Detail Admin"""
+    list_display = ["profile"]
