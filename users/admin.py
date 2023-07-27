4e5c08bccbf4bf73b5fffb7bf184b3244080a32b
Reza shakeri <rzashakeri@gmail.com>
2023-07-28 03:21:57 +0330

    ♻️ refactor: redesign admin

--- users/admin.py
+++ users/admin.py
@@ -1,53 +1,44 @@
 from django.contrib import admin
-from users.models import UserType, User, SeekerProfile, SeekerSkill, Skill, EducationDetail, ExperienceDetail, Company
+from users.models import (
+    JobSeeker,
+    Skill,
+    Education,
+    Experience,
+    Company,
+)
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    """User Admin"""
-    list_display = ["username", "first_name", "last_name", "gender", "user_type", "is_staff", "is_superuser"]
-    list_filter = ["is_staff", "is_superuser", "user_type", "gender", "date_joined"]
-    search_fields = ["first_name", "last_name", "username"]
-
-
-@admin.register(UserType)
-class UserTypeAdmin(admin.ModelAdmin):
-    """User Type Admin"""
-    pass
-
-
-@admin.register(SeekerProfile)
-class SeekerProfileAdmin(admin.ModelAdmin):
+@admin.register(JobSeeker)
+class JobSeekerAdmin(admin.ModelAdmin):
     """Seeker Profile Admin"""
-    list_display = ["__str__", "level", "birthday", "current_salary"]
-    list_filter = ["level", "birthday", "current_salary"]
-
-
-@admin.register(SeekerSkill)
-class SeekerSkillAdmin(admin.ModelAdmin):
-    """Seeker Skill Admin"""
-    list_display = ["profile", "skill"]
+    
+    list_display = ["__str__", "gender", "level", "location"]
+    list_filter = ["gender", "level", "location"]
 
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
-    """Skill Admin"""
-    pass
+    """Seeker Skill Admin"""
+    
+    list_display = ["name", "level"]
 
 
-@admin.register(EducationDetail)
-class EducationDetailAdmin(admin.ModelAdmin):
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
     """Education Detail Admin"""
+    
     list_display = ["profile"]
 
 
-@admin.register(ExperienceDetail)
-class ExperienceDetailAdmin(admin.ModelAdmin):
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
     """Experience Detail Admin"""
+    
     list_display = ["profile", "job_title", "company_name"]
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     """Experience Detail Admin"""
-    list_display = ["user", "name", "company_website_url", "is_verified"]
+    
+    list_display = ["user", "name", "website_url", "is_verified"]
