0a3203e1a3408957efb2b9139591a81e835d8bf4
Reza shakeri <rzashakeri@gmail.com>
2023-07-30 00:07:42 +0330

    ✨ feat: added LevelAdmin, SalaryAdmin, ExperienceAdmin and TypeAdmin to admin.py

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,15 +1,40 @@
 from django.contrib import admin
 
-from jobs.models import Job, Category
+from jobs.models import Job, Category, Level, Salary, Experience, Type
 
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     """Job Admin"""
-    pass
+    list_display = ["title", "company", "created_date", "location", "salary"]
+    list_filter = ["level", "experience", "level"]
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     """Category Admin"""
+    pass
+
+
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    """Level Admin"""
+    pass
+
+
+@admin.register(Salary)
+class SalaryAdmin(admin.ModelAdmin):
+    """Salary Admin"""
+    pass
+
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    """Salary Admin"""
+    pass
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    """Salary Admin"""
     pass
