6ada92db2cfe3d5f8ee524f7957164396678f0a2
Reza shakeri <rzashakeri@gmail.com>
2023-07-29 17:20:20 +0330

    ♻️ refactor: remove list_display and list_filter from JobAdmin

--- jobs/admin.py
+++ jobs/admin.py
@@ -6,8 +6,7 @@ from jobs.models import Job, Category
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     """Job Admin"""
-    list_display = ["title", "company", "created_date", "location", "level", "experience"]
-    list_filter = ["company", "created_date", "level", "experience"]
+    pass
 
 
 @admin.register(Category)
