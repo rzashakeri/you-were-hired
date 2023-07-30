bc0abe7e315e602a206bda394999ff14605ef98e
Reza shakeri <rzashakeri@gmail.com>
2023-07-30 17:05:39 +0330

    ♻️ refactor: normalize values

--- utils/choices.py
+++ utils/choices.py
@@ -2,37 +2,36 @@ from django.utils.translation import gettext_lazy as _
 
 # db constants =>
 GENDER_CHOICES = (
-    ("Male", _("Male")),
-    ("Female", _("Female")),
-    ("Not Specified", _("Not Specified")),
+    ("male", _("Male")),
+    ("female", _("Female")),
+    ("no matter", _("No matter")),
 )
 
 LEVEL_CHOICES = (
-    ("Senior", _("Senior")),
-    ("Mid-Level", _("Mid-Level")),
-    ("Junior", _("Junior")),
-    ("Intern", _("Intern")),
-    ("Not Specified", _("Not Specified")),
+    ("senior", _("Senior")),
+    ("mid-Level", _("Mid-Level")),
+    ("junior", _("Junior")),
+    ("intern", _("Intern")),
+    ("no matter", _("No matter")),
 )
 
 STATUS_CHOICES = (
-    ("Pending", _("Pending")),
-    ("Seen", _("Seen")),
-    ("Accepted", _("Accepted")),
+    ("pending", _("Pending")),
+    ("seen", _("Seen")),
+    ("accepted", _("Accepted")),
 )
 
 EXPERIENCE_CHOICES = (
-    ("Under 1 Year", _("Under 1 Year")),
-    ("1 - 2 Year", _("1 - 2 Year")),
-    ("3 - 4 Year", _("3 - 4 Year")),
-    ("5 - 6 Year", _("5 - 6 Year")),
-    ("Over 6 Years", _("Over 6 years")),
-    ("No Experience", _("No Experience")),
+    ("under 3 years", _("Under 3 Year")),
+    ("3 - 6 year", _("3 - 5 Year")),
+    ("over 6 years", _("Over 6 years")),
+    ("no matter", _("No matter")),
 )
 
 JOB_TYPE_CHOICES = (
-    ("Full Time Jobs", _("Full Time Jobs")),
-    ("Part Time Jobs", _("Part Time Jobs")),
-    ("Contract Jobs", _("Contract Jobs")),
-    ("Remote", _("Remote")),
+    ("full time jobs", _("Full Time Jobs")),
+    ("part time jobs", _("Part Time Jobs")),
+    ("contract jobs", _("Contract Jobs")),
+    ("remote", _("Remote")),
+    ("no matter", _("No matter")),
 )
