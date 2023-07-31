6e7e0d3b6ded2385eab84c9f7ab8e59005af569e
Reza shakeri <rzashakeri@gmail.com>
2023-07-31 03:59:19 +0330

    ✨ feat: complete implementation JobFilter

--- jobs/filters.py
+++ jobs/filters.py
@@ -1,16 +1,28 @@
 import django_filters
-from django.forms import SelectMultiple
 from django import forms
 from jobs.models import Job
 from utils.choices import JOB_TYPE_CHOICES, LEVEL_CHOICES, EXPERIENCE_CHOICES
+from dal import autocomplete
 
 
 class JobFilter(django_filters.FilterSet):
+    TAILWIND_CLASS = "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:border-gray-600"
+    
+    # This section for search =>
+    job_title = django_filters.CharFilter(
+        lookup_expr='icontains',
+        field_name="title",
+    )
+    job_location = django_filters.CharFilter(field_name="location__region__name", lookup_expr='icontains')
+    job_type = django_filters.CharFilter(field_name="type", lookup_expr='icontains')
+    job_experience = django_filters.CharFilter(field_name="experience", lookup_expr='icontains')
+    
+    # This section for filter =>
     type = django_filters.MultipleChoiceFilter(
         choices=JOB_TYPE_CHOICES,
         widget=forms.CheckboxSelectMultiple(
             attrs={
-                "class": "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:border-gray-600"
+                "class": TAILWIND_CLASS
             }
         )
     )
@@ -18,7 +30,7 @@ class JobFilter(django_filters.FilterSet):
         choices=LEVEL_CHOICES,
         widget=forms.CheckboxSelectMultiple(
             attrs={
-                "class": "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:border-gray-600"
+                "class": TAILWIND_CLASS
             }
         )
     )
@@ -26,7 +38,7 @@ class JobFilter(django_filters.FilterSet):
         choices=EXPERIENCE_CHOICES,
         widget=forms.CheckboxSelectMultiple(
             attrs={
-                "class": "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:border-gray-600"
+                "class": TAILWIND_CLASS
             }
         )
     )
@@ -37,7 +49,4 @@ class JobFilter(django_filters.FilterSet):
         ),
         empty_label=None
     )
-    
-    class Meta:
-        model = Job
-        fields = ["type", "level", "experience"]
+
