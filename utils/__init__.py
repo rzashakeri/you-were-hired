a46e93ada18e2403db013454d96c55ed381c0152
Reza shakeri <rzashakeri@gmail.com>
2023-08-03 00:16:20 +0330

    ♻️ refactor: get extensions file with pathlib

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,8 @@
+import pathlib
+
+
 def logo_directory_path(instance, filename):
-    file_name, file_extensions = filename.split('.')
+    file_extensions = pathlib.Path(filename).suffix
     file_name = instance.slug
     path = "company/logo/images/"
     return "{0}/{1}.{2}".format(path, file_name, file_extensions)
