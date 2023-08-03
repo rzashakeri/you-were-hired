5df6733a6df96b2b4adb679b59b8b2d277279fe3
Reza shakeri <rzashakeri@gmail.com>
2023-08-03 04:27:48 +0330

    ✨ feat: added SkillAdmin to admin.py

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
 
-from jobs.models import Job, Category
+from jobs.models import Job, Category, Skill
 
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     """Job Admin"""
     list_display = ["title", "company", "created_date", "location", "salary"]
-    list_filter = ["level", "experience", "level", "type", "category"]
+    list_filter = ["level", "experience", "level", "type"]
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     """Category Admin"""
     pass
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    """Seeker Skill Admin"""
+    
+    list_display = ["name", "level"]
 
